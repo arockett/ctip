1ac105b7efa3ae4c531fdcc8a626ab47d86e0192
Aaron Beckett <aminor65ii@gmail.com>
2016-09-07 13:44:29 -0400

    Write parameterized test for gen schema read function.

--- tests/test_gen_schema_reading_and_writing.py
+++ tests/test_gen_schema_reading_and_writing.py
@@ -9,23 +9,40 @@ Created on Sun Jul 10 19:54:47 2016
 
 
 import pytest
+import json
 
 from ctip import GenSchema
 
 
+
 def gather_test_files():
     """Search the tests/resources directory for pairs of gen and config files."""
-    pass
+    return [
+        ("tests/resources/genfile1_single_var_single_arg.gen", "tests/resources/configs1.json"),
+        ("tests/resources/genfile2_single_var_multiple_args.gen", "tests/resources/configs2.json"),
+        ("tests/resources/genfile3_multiple_vars.gen", "tests/resources/configs3.json"),
+        ("tests/resources/genfile4_simple_nested_preconstructed_args.gen", "tests/resources/configs4.json"),
+        ("tests/resources/genfile5_simple_nested.gen", "tests/resources/configs5.json"),
+        ("tests/resources/genfile6_multiple_vars_in_nest.gen", "tests/resources/configs6.json"),
+        ("tests/resources/genfile7_incomplete_nested.gen", "tests/resources/configs7.json"),
+        ("tests/resources/genfile8_multiple_nests.gen", "tests/resources/configs8.json"),
+        ("tests/resources/genfile9_multi_nested.gen", "tests/resources/configs9.json"),
+        ("tests/resources/genfile10_multiple_vars_own_nest.gen", "tests/resources/configs10.json"),
+        ("tests/resources/genfile11_commented", "tests/resources/configs11.json")
+    ]
 
 
-@pytest.mark.skip
-class TestGenSchemaReader(object):
-    """Tests for Genfile parsing method."""
-    pass
+@pytest.mark.parametrize("genfile,config_file", gather_test_files())
+def test_gen_schema_read(genfile, config_file):
+    """Test the Genfile read method with all examples in test/resources."""
+    schema = GenSchema.read(genfile)
+    configs = json.load(open(config_file))
+
+    pytest.helpers.compare_configs(configs, schema)
 
 
-@pytest.mark.skip
-class TestGenSchemaWriter(object):
-    """Tests for the Genfile writing method."""
+@pytest.mark.parametrize("genfile,config_file", gather_test_files())
+def test_gen_schema_write(genfile, config_file):
+    """Test the Genfile write method with all examples in test/resources."""
     pass
 
