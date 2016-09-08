ffec5984078f42582998db4d15b1c4b42f9fbf32
Aaron Beckett <aminor65ii@gmail.com>
2016-09-08 11:42:24 -0400

    Add tests for range reading and writing.

--- tests/test_gen_schema_reading_and_writing.py
+++ tests/test_gen_schema_reading_and_writing.py
@@ -17,19 +17,18 @@ from ctip import GenSchema
 
 def gather_test_files():
     """Search the tests/resources directory for pairs of gen and config files."""
-    return [
-        ("tests/resources/genfile1_single_var_single_arg.gen", "tests/resources/configs1.json"),
-        ("tests/resources/genfile2_single_var_multiple_args.gen", "tests/resources/configs2.json"),
-        ("tests/resources/genfile3_multiple_vars.gen", "tests/resources/configs3.json"),
-        ("tests/resources/genfile4_simple_nested_preconstructed_args.gen", "tests/resources/configs4.json"),
-        ("tests/resources/genfile5_simple_nested.gen", "tests/resources/configs5.json"),
-        ("tests/resources/genfile6_multiple_vars_in_nest.gen", "tests/resources/configs6.json"),
-        ("tests/resources/genfile7_incomplete_nested.gen", "tests/resources/configs7.json"),
-        ("tests/resources/genfile8_multiple_nests.gen", "tests/resources/configs8.json"),
-        ("tests/resources/genfile9_multi_nested.gen", "tests/resources/configs9.json"),
-        ("tests/resources/genfile10_multiple_vars_own_nest.gen", "tests/resources/configs10.json"),
-        ("tests/resources/genfile11_commented.gen", "tests/resources/configs11.json")
-    ]
+
+    resource_dir = "tests" + os.path.sep + "resources" + os.path.sep
+
+    genfile_config_pairs = []
+    for (dirpath, dirnames, filenames) in os.walk(resource_dir):
+        for name in filenames:
+            if name.startswith("genfile"):
+                cfg_name = name.split("_")[0].replace("genfile", "configs") + ".json"
+                pair = (resource_dir + name, resource_dir + cfg_name)
+                genfile_config_pairs.append(pair)
+        break
+    return genfile_config_pairs
 
 
 @pytest.mark.parametrize("genfile,config_file", gather_test_files())
